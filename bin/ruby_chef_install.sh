bf2285127e9a2d137dfaf378ba10e37f215e166b
Ross Kaffenberger <rosskaff@gmail.com>
2013-11-29 12:15:43 -0500

    mina setup

--- bin/ruby_chef_install.sh
+++ bin/ruby_chef_install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-RUBY_VERSION=ruby-1.9.3-p484
+RUBY_VERSION=ruby-2.0.0-p247
 apt-get -y update
 apt-get -y install build-essential libyaml-dev zlib1g-dev openssl libssl-dev libreadline-gplv2-dev
 cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/$RUBY_VERSION.tar.gz
+wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/$RUBY_VERSION.tar.gz
 tar -xvzf $RUBY_VERSION.tar.gz
 cd $RUBY_VERSION/
 sudo ./configure --prefix=/usr/local
 sudo make
 sudo make install
-gem install chef ruby-shadow --no-ri --no-rdoc
+gem install ruby-shadow --no-ri --no-rdoc
